1ed7a29745aec389e6150922f1e6c1af208dc123
Daniyar Bulegenov <purap1e@mail.ru>
2023-12-17 18:28:55 +0600

    elastic deleted

--- env-vars.sh
+++ env-vars.sh
@@ -1,4 +1,4 @@
-export ELASTICSEARCH_HOST=http://elasticsearch
-export ELASTICSEARCH_PORT=9200
-export ELASTICSEARCH_USERNAME=elastic
-export ELASTICSEARCH_PASSWORD=elastic
+export POSTGRES_DB=postgres
+export DATASOURCE_URL=jdbc:postgresql://db:5432/${POSTGRES_DB}
+export DATASOURCE_USERNAME=postgres
+export DATASOURCE_PASSWORD=postgres
